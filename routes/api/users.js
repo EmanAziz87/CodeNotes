646f969eed5d8a7dabb4b714d91f628d7ec3de47
Eman <aziz.eman@hotmail.com>
2024-01-01 18:51:18 -0500

    working on implementing express-validator validation to allow custom errors and ensure interactions with end point of our apis is not possible -- beginning stages

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult, matchedData } = require("express-validator");
 
-//@route          GET api/post
-//@description    test route
+//@route          POST api/users
+//@description    Register user
 //@access         public (Token Required)
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Users route");
-});
+router.get(
+  "/",
+  [
+    check("person", "You must provide a name").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // res.send("Users route");
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      return res.status(400).send({ errors: result.array() });
+    }
+
+    const data = matchedData(req);
+    res.send(
+      `Hello, ${data.person}! email: ${email} --- password: ${password}`
+    );
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 app.get("/", (req, res) => res.send("API Running"));
 
 //Initialize Middleware
-app.use(express.json({ extended: false }));
+
+app.use(express.json({ extended: false })); //parses data being sent to new body object of request object
 
 //Defining Routes
+//first param is the root root address params, the second is requiring
+//the router of each respective route.
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
